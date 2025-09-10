6f2e74aa1d0989411f4864bd42a381fe64db4b61
Pradeepa-Ramanathan <pradeepadeepa966@gmail.com>
2025-09-10 18:29:13 +0530

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+// src/components/Navbar.jsx
+import React from 'react';
+import { FaPhone, FaEnvelope, FaFacebookF, FaInstagram, FaYoutube, FaSearch } from 'react-icons/fa';
+
+const Navbar = () => {
+  return (
+    <header>
+      {/* Top Contact Bar */}
+      <div style={{ backgroundColor: '#E8EEF1', color: '#1E3D58' }} className="py-2 px-3 d-flex justify-content-between align-items-center">
+        <div className="d-flex gap-3">
+          <span><FaPhone className="me-1" /> +1-234-5678</span>
+          <span><FaEnvelope className="me-1" /> ravuequeries@gmail.com</span>
+        </div>
+        <div className="d-flex gap-3">
+          <a href="#" style={{ color: '#1E3D58' }}><FaFacebookF /></a>
+          <a href="#" style={{ color: '#1E3D58' }}><FaInstagram /></a>
+          <a href="#" style={{ color: '#1E3D58' }}><FaYoutube /></a>
+        </div>
+      </div>
+
+      {/* Main Navbar */}
+      <nav style={{ backgroundColor: '#057DCD' }} className="navbar navbar-expand-lg px-3">
+        <div className="container-fluid">
+          {/* Logo */}
+          <a className="navbar-brand d-flex align-items-center text-white" href="#">
+            <FaSearch className="me-2" style={{ color: '#43B0F1' }} />
+            <strong>RAVUE</strong>
+          </a>
+
+          {/* Toggler for mobile */}
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Nav Links */}
+          <div className="collapse navbar-collapse" id="mainNavbar">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item"><a className="nav-link text-white" href="#">Home</a></li>
+              <li className="nav-item"><a className="nav-link text-white" href="#">About Us</a></li>
+              <li className="nav-item"><a className="nav-link text-white" href="#">Get review</a></li>
+              <li className="nav-item"><a className="nav-link text-white" href="#">Contact Us</a></li>
+            </ul>
+
+            {/* Login Button */}
+            <button style={{ backgroundColor: '#e8eef1', color: 'black' }} className="btn">
+              Login
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
